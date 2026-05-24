d22778fdcf2017dbb2759f591127c0d9503e4926
Ivan Sablin <chaharminga@gmail.com>
2026-05-24 23:54:46 +0400

    BCORE-14: Переходим на Spring Boot на порту 8081

--- src/main/java/ru/mentee/power/crm/Main.java
+++ src/main/java/ru/mentee/power/crm/Main.java
@@ -1,5 +1,7 @@
 package ru.mentee.power.crm;
 
+import java.io.File;
+
 import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
 import ru.mentee.power.crm.infrastructure.InMemoryLeadRepository;
@@ -7,8 +9,6 @@ import ru.mentee.power.crm.model.LeadStatus;
 import ru.mentee.power.crm.repository.LeadRepository;
 import ru.mentee.power.crm.service.LeadService;
 import ru.mentee.power.crm.servlet.LeadListServlet;
-
-import java.io.File;
 
 public class Main {
 
